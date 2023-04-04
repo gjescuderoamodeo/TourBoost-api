4ac179ea7e8e72ec315fe2b9cd9f07f0c3a70cd7
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-04-04 14:59:02 +0200

    add bcryptjs

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 require('dotenv').config();
 
+//encriptador contraseña
+const bcrypt = require('bcryptjs');
+
 const prisma = new PrismaClient();
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -21,19 +24,25 @@ app.get('/usuario', async (req, res) => {
 
 //crear usuarios
 app.post('/usuario', async (req, res) => {
-    const { nombre, apellidos, password, correo, admin } = req.body
+    const { nombre, apellidos, password, correo, admin } = req.body;
+  
+    // Generar una cadena aleatoria para utilizar como salt (número de rondas de hashing)
+    const salt = await bcrypt.genSalt(10);
+  
+    // Hash de la contraseña
+    const hashPassword = await bcrypt.hash(password, salt);
   
     const nuevoUsuario = await prisma.usuario.create({
       data: {
         nombre,
         apellidos,
-        password,
+        password: hashPassword, // Guardar el hash de la contraseña en la base de datos
         correo,
         admin
       }
     })
   
-    res.json(nuevoUsuario)
+    res.json(nuevoUsuario);
   })
   
 
