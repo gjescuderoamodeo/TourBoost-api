ff7666b9dd8e99f9157c27c4d9bec9ca42a4266c
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-04-06 15:26:07 +0200

    Update schema.prisma
    añadido recomendación a lugar

--- api/users.js
+++ api/users.js
@@ -1,6 +1,8 @@
 import { instanciaSingleton } from "../prisma/prisma.js";
+import bcrypt from 'bcrypt';
 
 const prisma = instanciaSingleton;
+
 
 // Obtener todos los usuarios
 async function obtenerUsuarios(req, res) {
@@ -15,17 +17,25 @@ async function obtenerUsuarios(req, res) {
 
 // Crear un nuevo usuario
 async function crearUsuario(req, res) {
-  const { nombre, email, contrasena } = req.body;
+  const { nombre, apellidos, password, correo, admin } = req.body;
+  
+  // Generar una cadena aleatoria para utilizar como salt (número de rondas de hashing)
+  const salt = await bcrypt.genSalt(10);
 
   try {
-    const usuario = await prisma.usuario.create({
-      data: {
-        nombre,
-        email,
-        contrasena,
-      },
-    });
-    res.json(usuario);
+  // Hash de la contraseña
+  const hashPassword = await bcrypt.hash(password, salt);
+
+    const nuevoUsuario = await prisma.usuario.create({
+        data: {
+          nombre,
+          apellidos,
+          password: hashPassword, // Guardar el hash de la contraseña en la base de datos
+          correo,
+          admin
+        }
+      });
+    res.json(nuevoUsuario);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "No se pudo crear el usuario" });
