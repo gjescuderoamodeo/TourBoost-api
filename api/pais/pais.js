ee9f5f0a5cdec519015bbb0bf260d004b34014db
gjescuderoamodeo <gjescuderoamodeo@gmail.com>
2023-05-05 10:07:46 +0200

    Update pais.js
    add delete pais

--- api/pais/pais.js
+++ api/pais/pais.js
@@ -15,6 +15,23 @@ async function obtenerPaises(req, res) {
   }
 }
 
+//borrar un pais
+async function borrarPais(req, res) {
+  const { nombre } = req.body;
+
+  try {
+    const pais = await prisma.pais.deleteMany({
+      where: {
+        nombre: nombre,
+      },
+    });
+    res.json({ message: `El pais con nombre ${nombre} ha sido eliminado correctamente` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: `No se pudo eliminar el pais con nombre ${nombre}` });
+  }
+}
+
 // Crear un nuevo pais
 async function crearPais(req, res) {
   const { codigo_pais, nombre} = req.body;
